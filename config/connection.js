ba788ccbe14d4021b81c212e1f7b8a090f128a56
Justin Hill <sale1@Justins-MBP.lan>
2024-05-06 22:08:44 -0700

    initial commit

---
+++ config/connection.js
@@ -0,0 +1,14 @@
+const Sequelize = require('sequelize');
+require('dotenv').config(); // Import sensitive data from .env
+
+const sequelize = process.env.JAWSDB_URL 
+  ? new Sequelize(process.env.JAWSDB_URL) // For HEROKU
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, { // This is for Local
+      host: process.env.DB_HOST,
+      dialect: 'mysql',
+      dialectOptions: {
+        decimalNumbers: true,
+      },
+    });
+
+module.exports = sequelize;
